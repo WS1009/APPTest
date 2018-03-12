c28b027e36e21f134049e511b5ed4be0a3bddf56
ws <WANGSHUN611@163.com>
2018-03-12 22:56:51 +0800

    1

--- app/src/main/java/com/wangshun/apptest/http/OkHttpActivity.java
+++ app/src/main/java/com/wangshun/apptest/http/OkHttpActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.wangshun.apptest.R;
+
+import java.io.IOException;
 
 public class OkHttpActivity extends Activity {
 
@@ -19,19 +25,42 @@ public class OkHttpActivity extends Activity {
         setContentView(R.layout.activity_okhttp);
 
         mTv = findViewById(R.id.tv);
-        OkHttpClientManager.getAsyn("https://www.baidu.com", new OkHttpClientManager.ResultCallback<String>()
-        {
+
+        //封装
+        OkHttpClientManager.getAsyn("https://www.baidu.com", new OkHttpClientManager.ResultCallback<String>() {
             @Override
-            public void onError(Request request, Exception e)
-            {
+            public void onError(Request request, Exception e) {
                 e.printStackTrace();
             }
 
             @Override
-            public void onResponse(String str)
-            {
+            public void onResponse(String str) {
                 mTv.setText(str);//注意这里是UI线程
             }
         });
+
+
+
+        //原生：创建okHttpClient对象
+        OkHttpClient mOkHttpClient = new OkHttpClient();
+        //创建一个Request
+        final Request request = new Request.Builder()
+                .url("https://www.baidu.com")
+                .build();
+        //new call
+        Call call = mOkHttpClient.newCall(request);
+        //请求加入调度
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+            }
+
+            @Override
+            public void onResponse(final Response response) throws IOException {
+                String htmlStr =  response.body().string();
+                System.out.println("htmlStr: "+htmlStr);
+            }
+        });
+
     }
 }
